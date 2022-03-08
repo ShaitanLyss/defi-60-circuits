fdca749a6e8cd68e05630619635178aceecf2804
MoonLyss <shaitan.lyssm@gmail.com>
2022-03-08 15:20:06 +0100

    Add place holder weather update every x seconds

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
+// Consts
+const changeCityIntervalInSeconds = 5;
+
 // Server setup
 const express = require('express');
 const app = express();
@@ -6,12 +9,22 @@ const server = http.createServer(app);
 const {Server} = require("socket.io");
 const io = new Server(server);
 
+
 // Page content
 app.get('/', (req, res) => {
     res.send("<h1>Weather report</h1>");
 });
 
+/**
+ * Changes the displayed weather to a random city.
+ */
+function changeDisplayedCityWeather() {
+    console.log('Change displayed city');
+}
 
+// Set the city change to occur every x seconds
+setInterval(changeDisplayedCityWeather,
+    changeCityIntervalInSeconds * 1000);
 
 
 // Server start
